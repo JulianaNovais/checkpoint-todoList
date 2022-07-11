1d5420d664c2be33d2f4c7190867f12f9996d5fc
Gabrielle Rodrigues <gabrielle123@hotmail.com>
2022-07-10 23:38:48 -0300

    CheckPoint 2022

---
+++ scripts/animations/spinner_loader.js
@@ -0,0 +1,17 @@
+// function exibeSpinner(){
+//     let div = document.createElement("div");
+//     div.classList.add("loader"); 
+
+//     let buttonLogin = document.getElementById("buttonLogin"); 
+//     buttonLogin.innerText = ""; 
+//     buttonLogin.appendChild(div); 
+
+
+// }
+
+// function ocultaSpinner(){
+//     let buttonLogin = document.getElementById("buttonLogin");
+//     let div = document.querySelector(".loader"); 
+//     buttonLogin.removeChild(div); 
+
+// }
